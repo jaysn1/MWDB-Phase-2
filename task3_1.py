42b8683c03cabd30f81da915493fba43faf053eb
jaysn1 <jay.nankani1@gmail.com>
2020-10-15 20:38:16 -0700

    gesture-gesture for DTW

--- task3_1.py
+++ task3_1.py
@@ -7,8 +7,13 @@ Created on Thu Oct 15 12:56:53 2020
 import json
 from similarity_calculators.dot_product_similarity import dot_product_similarity
 from similarity_calculators.edit_distance_similarity import edit_distance_similarity
+from similarity_calculators.dtw_similarity import dynamic_time_warping
+
 from tqdm import tqdm
 import concurrent.futures
+from read_word_average_dict import read_word_average_dict
+from read_sensor_average_std_dict import read_sensor_average_std_dict
+
 
 def get_query_for_edit_distance(components, sensors, gesture_id, word_store, s):
     query = {}
@@ -48,6 +53,23 @@ def edit_distance_thread(p_vectors, vectors, components, sensors, mega_data_stor
                 gesture_similarity[gesture] = float("inf")
         thread_gestures[gesture_id] = gesture_similarity
     return thread_gestures
+
+def DTW_thread(p_words, word_average_dict, sensor_avg_std_dict):
+    thread_gestures = {}
+    for gesture_id in tqdm(p_words):
+        gesture_similarity = {}
+        for gesture in word_average_dict:
+            gesture_similarity[gesture] = 0
+            for components_id in word_average_dict[gesture_id]:
+                for sensor_id in word_average_dict[gesture_id][components_id]:
+                    weight = abs(sensor_avg_std_dict[gesture][components_id][sensor_id][0] - sensor_avg_std_dict[gesture_id][components_id][sensor_id][0])
+                    gesture_similarity[gesture] += dynamic_time_warping(word_average_dict[gesture_id][components_id][sensor_id],word_average_dict[gesture][components_id][sensor_id], weight)
+            if gesture_similarity[gesture]==0:
+                gesture_similarity[gesture] = float("inf")
+            else:
+                gesture_similarity[gesture] = (gesture_similarity[gesture])**-1
+        thread_gestures[gesture_id] = gesture_similarity
+    return thread_gestures
     
 def main():
     print("""
@@ -71,6 +93,8 @@ def main():
     gesture_gesture_similarity_dir="intermediate/gesture_gesture_similarity_dictionary.json"
     parameters_path="intermediate/data_parameters.json"
     word_vector="intermediate/word_vector_dict.json"
+    word_average_dict_dir="intermediate/word_avg_dict.json"
+    sensor_average_std_dict_dir = "intermediate/sensor_avg_std_dict.json"
     with open(vectors_dir) as f:
         vectors = json.load(f)
     
@@ -118,7 +142,44 @@ def main():
             gesture_gesture_similarity.update(f.result())
         with open(gesture_gesture_similarity_dir, "w") as write_file:
             json.dump(gesture_gesture_similarity, write_file)
+    
+    elif user_option == 7:
+        threads = 10
+        word_average_dict = read_word_average_dict(word_average_dict_dir)
+        sensor_avg_std_dict = read_sensor_average_std_dict(sensor_average_std_dict_dir)
         
+        gesture_gesture_similarity = {}
+        
+        for gesture_id in tqdm(word_average_dict):
+            gesture_similarity = {}
+            for gesture in word_average_dict:
+                gesture_similarity[gesture] = 0
+                for components_id in word_average_dict[gesture_id]:
+                    for sensor_id in word_average_dict[gesture_id][components_id]:
+                        weight = abs(sensor_avg_std_dict[gesture][components_id][sensor_id][0] - sensor_avg_std_dict[gesture_id][components_id][sensor_id][0])
+                        gesture_similarity[gesture] += dynamic_time_warping(word_average_dict[gesture_id][components_id][sensor_id],word_average_dict[gesture][components_id][sensor_id], weight)
+                if gesture_similarity[gesture]==0:
+                    gesture_similarity[gesture] = float("inf")
+                else:
+                    gesture_similarity[gesture] = (gesture_similarity[gesture])**-1
+            gesture_gesture_similarity[gesture_id] = gesture_similarity
+        
+        # partitioned_words = []
+        # for i in range(threads):
+        #     j = i*len(word_average_dict)//threads
+        #     partitioned_words.append(dict(list(word_average_dict.items())[j:j+len(word_average_dict)//threads]))
+        # partitioned_words.append(dict(list(word_average_dict.items())[threads*(len(word_average_dict)//threads):]))
+        
+        
+        # with concurrent.futures.ThreadPoolExecutor() as executor:
+        #     future = [executor.submit(DTW_thread, p_words, word_average_dict, sensor_avg_std_dict) for p_words in tqdm(partitioned_words)]
+       
+        # for f in future:
+        #     gesture_gesture_similarity.update(f.result())
+        
+        with open(gesture_gesture_similarity_dir, "w") as write_file:
+            json.dump(gesture_gesture_similarity, write_file)
+        return gesture_gesture_similarity
 c = main()
             
             
