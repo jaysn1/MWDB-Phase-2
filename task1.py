17bbd1c94a4c0b7ce264832d1512bbf9cd35e454
Monil Nisar <nisarmonil@gmail.com>
2020-11-13 00:45:52 -0700

    Added Task2: KNN

--- task1.py
+++ task1.py
@@ -14,8 +14,9 @@ for a personalized PageRank formulation based on RandomWalks with re-start.
 author: Monil
 TODO: Visualise < m > dominant gestures?
 """
-from Phase2 import task0a, task0b
+from Phase2 import task0a, task0b, task1 as phase2task1
 from Phase2.similarity_calculators.dot_product_similarity import dot_product_similarity
+from Phase2.similarity_calculators.distance_for_PCA_SVD_NMF_LDA import calculate_similarity
 from Phase1.helper import min_max_scaler, load_data
 from visualize import visualize
 
@@ -91,6 +92,114 @@ def pagerank(graph, beta, topic, maxiter = 1000):
     
     return i
 
+def task1_initial_setup(user_option, vector_model=0, create_vectors=False):
+    """
+    """
+    # need to run task0, 3 from pahse 2
+    if create_vectors:
+        data = {}
+        data['directory'] = input("Enter directory: ")
+        data['window_length'] = int(input("Enter window length: "))
+        data['shift_length'] = int(input("Enter shift length: "))
+        data['resolution'] = int(input("Enter resolution: "))
+        
+        task0a.main(data)
+        task0b.main()
+    #############################################  creating gesture-gesture matrix  #####################################
+    mapping = {1:'DOT', 2: 'PCA', 3: 'SVD', 4: 'NMF', 5: 'LDA', 6: 'ED', 7: 'DTW'}
+    vectors_dir = "intermediate/vectors_dictionary.json"
+    parameters_path = "intermediate/data_parameters.json"
+    word_vector = "intermediate/word_vector_dict.json"
+    word_average_dict_dir = "intermediate/word_avg_dict.json"
+    sensor_average_std_dict_dir = "intermediate/sensor_avg_std_dict.json"
+    gesture_gesture_similarity_dir = "intermediate/{}_gesture_gesture_similarity_dictionary.pk".format(mapping[user_option])
+    k = 4     # top components
+    
+    if not os.path.exists(gesture_gesture_similarity_dir):
+        with open(vectors_dir) as f:
+            vectors = json.load(f)
+        gesture_gesture_similarity = {}
+        if user_option == 1:
+            for gesture_id in vectors:
+                vector = vectors[gesture_id][vector_model - 1]
+                gesture_similarity = {}
+                for gesture in vectors:
+                    gesture_similarity[gesture] = dot_product_similarity(vector, vectors[gesture][vector_model - 1])
+                gesture_gesture_similarity[gesture_id] = gesture_similarity
+
+        elif user_option == 2 or user_option == 3 or user_option == 4 or user_option == 5:
+            transformed_data_dir = "intermediate/{}_{}_transformed_data.json".format(mapping[user_option], vector_model)
+            
+            if not os.path.exists(transformed_data_dir):
+                print("Creating transformed vectors.")
+                phase2task1.main(user_option-1, vector_model, k)
+                
+            with open(transformed_data_dir, "r") as f:
+                transformed_data = json.load(f)
+
+            for gesture_id in transformed_data:
+                query = transformed_data[gesture_id]
+                gesture_similarity = {}
+                for gesture, gesture_data in transformed_data.items():
+                    gesture_similarity[gesture] = calculate_similarity(query, gesture_data)
+                gesture_gesture_similarity[gesture_id] = gesture_similarity        
+
+        elif user_option == 6:
+            threads = 1
+            # Initialize default config
+            with open(parameters_path) as f:
+                parameters = json.load(f)
+            s = parameters['shift_length']
+            components = ['X', 'Y', 'Z', 'W']
+            sensors = 20
+
+            # Open the word vector from task 0
+            with open(word_vector) as f:
+                word_store = json.load(f)
+
+            mega_data_store = {}
+            for gesture_id in vectors:
+                mega_data_store[gesture_id] = get_query_for_edit_distance(components, sensors, gesture_id, word_store, s)
+
+            partitioned_vectors = []
+            for i in range(threads):
+                j = i * len(vectors) // threads
+                partitioned_vectors.append(list(vectors.keys())[j:j + len(vectors) // threads])
+            partitioned_vectors.append(list(vectors.keys())[threads * (len(vectors) // threads):])
+
+            with concurrent.futures.ThreadPoolExecutor() as executor:
+                future = [
+                    executor.submit(edit_distance_thread, p_vector, vectors.keys(), components, sensors, mega_data_store, s)
+                    for p_vector in partitioned_vectors]
+
+            for f in future:
+                gesture_gesture_similarity.update(f.result())
+
+        elif user_option == 7:
+            word_average_dict = read_word_average_dict(word_average_dict_dir)
+            sensor_avg_std_dict = read_sensor_average_std_dict(sensor_average_std_dict_dir)
+
+            for gesture_id in tqdm(word_average_dict):
+                gesture_similarity = {}
+                for gesture in word_average_dict:
+                    gesture_similarity[gesture] = 1
+                    for components_id in word_average_dict[gesture_id]:
+                        for sensor_id in word_average_dict[gesture_id][components_id]:
+                            weight = abs(sensor_avg_std_dict[gesture][components_id][sensor_id][0] -
+                                         sensor_avg_std_dict[gesture_id][components_id][sensor_id][0])
+                            gesture_similarity[gesture] += dynamic_time_warping(
+                                word_average_dict[gesture_id][components_id][sensor_id],
+                                word_average_dict[gesture][components_id][sensor_id], weight)
+                    gesture_similarity[gesture] = 1/gesture_similarity[gesture]
+                gesture_gesture_similarity[gesture_id] = gesture_similarity
+
+        with open(gesture_gesture_similarity_dir, "wb") as write_file:
+            pickle.dump(gesture_gesture_similarity, write_file)
+    else:
+        with open(gesture_gesture_similarity_dir, "rb") as read_file:
+            gesture_gesture_similarity = pickle.load(read_file)
+    return gesture_gesture_similarity
+
 def main(gesture_gesture_matrix, k, m, seed_nodes, beta = 0.8, **kwargs):
     """
     input: k, m, (gestures to create gesture-gesture matrix), n seed nodes
@@ -157,110 +266,7 @@ def main(gesture_gesture_matrix, k, m, seed_nodes, beta = 0.8, **kwargs):
         plt.show()
     return top_m_ppr
 
-def task1_initial_setup(user_option, vector_model=0, create_vectors=False):
-    """
-    """
-    # need to run task0, 3 from pahse 2
-    if create_vectors:
-        data = {}
-        data['directory'] = input("Enter directory: ")
-        data['window_length'] = int(input("Enter window length: "))
-        data['shift_length'] = int(input("Enter shift length: "))
-        data['resolution'] = int(input("Enter resolution: "))
-        
-        task0a.main(data)
-        task0b.main()
-    #############################################  creating gesture-gesture matrix  #####################################
-    mapping = {1:'DOT', 2: 'PCA', 3: 'SVD', 4: 'NMF', 5: 'LDA', 6: 'ED', 7: 'DTW'}
-    vectors_dir = "intermediate/vectors_dictionary.json"
-    parameters_path = "intermediate/data_parameters.json"
-    word_vector = "intermediate/word_vector_dict.json"
-    word_average_dict_dir = "intermediate/word_avg_dict.json"
-    sensor_average_std_dict_dir = "intermediate/sensor_avg_std_dict.json"
-    gesture_gesture_similarity_dir = "intermediate/{}_gesture_gesture_similarity_dictionary.pk".format(mapping[user_option])
-    
-    if not os.path.exists(gesture_gesture_similarity_dir):
-        with open(vectors_dir) as f:
-            vectors = json.load(f)
-        gesture_gesture_similarity = {}
-        if user_option == 1:
-            for gesture_id in vectors:
-                vector = vectors[gesture_id][vector_model - 1]
-                gesture_similarity = {}
-                for gesture in vectors:
-                    gesture_similarity[gesture] = dot_product_similarity(vector, vectors[gesture][vector_model - 1])
-                gesture_gesture_similarity[gesture_id] = gesture_similarity
 
-        elif user_option == 2 or user_option == 3 or user_option == 4 or user_option == 5:
-            transformed_data_dir = "intermediate/{}_{}_transformed_data.json".format(mapping[user_option], vector_model)
-            
-            if not os.path.exists(transformed_data_dir):
-                raise ValueError("Please run task 1 with proper inputs.")
-            with open(transformed_data_dir, "r") as f:
-                transformed_data = json.load(f)
-
-            for gesture_id in transformed_data:
-                query = transformed_data[gesture_id]
-                gesture_similarity = {}
-                for gesture, gesture_data in transformed_data.items():
-                    gesture_similarity[gesture] = calculate_similarity(query, gesture_data)
-                gesture_gesture_similarity[gesture_id] = gesture_similarity        
-
-        elif user_option == 6:
-            threads = 1
-            # Initialize default config
-            with open(parameters_path) as f:
-                parameters = json.load(f)
-            s = parameters['shift_length']
-            components = ['X', 'Y', 'Z', 'W']
-            sensors = 20
-
-            # Open the word vector from task 0
-            with open(word_vector) as f:
-                word_store = json.load(f)
-
-            mega_data_store = {}
-            for gesture_id in vectors:
-                mega_data_store[gesture_id] = get_query_for_edit_distance(components, sensors, gesture_id, word_store, s)
-
-            partitioned_vectors = []
-            for i in range(threads):
-                j = i * len(vectors) // threads
-                partitioned_vectors.append(list(vectors.keys())[j:j + len(vectors) // threads])
-            partitioned_vectors.append(list(vectors.keys())[threads * (len(vectors) // threads):])
-
-            with concurrent.futures.ThreadPoolExecutor() as executor:
-                future = [
-                    executor.submit(edit_distance_thread, p_vector, vectors.keys(), components, sensors, mega_data_store, s)
-                    for p_vector in partitioned_vectors]
-
-            for f in future:
-                gesture_gesture_similarity.update(f.result())
-
-        elif user_option == 7:
-            word_average_dict = read_word_average_dict(word_average_dict_dir)
-            sensor_avg_std_dict = read_sensor_average_std_dict(sensor_average_std_dict_dir)
-
-            for gesture_id in tqdm(word_average_dict):
-                gesture_similarity = {}
-                for gesture in word_average_dict:
-                    gesture_similarity[gesture] = 1
-                    for components_id in word_average_dict[gesture_id]:
-                        for sensor_id in word_average_dict[gesture_id][components_id]:
-                            weight = abs(sensor_avg_std_dict[gesture][components_id][sensor_id][0] -
-                                         sensor_avg_std_dict[gesture_id][components_id][sensor_id][0])
-                            gesture_similarity[gesture] += dynamic_time_warping(
-                                word_average_dict[gesture_id][components_id][sensor_id],
-                                word_average_dict[gesture][components_id][sensor_id], weight)
-                    gesture_similarity[gesture] = 1/gesture_similarity[gesture]
-                gesture_gesture_similarity[gesture_id] = gesture_similarity
-
-        with open(gesture_gesture_similarity_dir, "wb") as write_file:
-            pickle.dump(gesture_gesture_similarity, write_file)
-    else:
-        with open(gesture_gesture_similarity_dir, "rb") as read_file:
-            gesture_gesture_similarity = pickle.load(read_file)
-    return gesture_gesture_similarity
 
 if __name__=="__main__":
 
---
+++ task2.py
@@ -0,0 +1,71 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Nov 12 22:52:18 2020
+
+@author: Jaysn
+"""
+from Phase2 import task1
+import pandas as pd
+
+
+
+def read_labels(labels_dir):
+    labels = pd.read_excel(labels_dir, header=None)
+    labels.columns = ['id', 'label']
+    return labels
+
+def calculate_distance_knn(query_values, gesture_values):
+    distance = 0
+    for query_word, gesture_word in zip(query_values, gesture_values):
+        distance += (query_word - gesture_word) ** 2
+    return distance ** 0.5
+
+def calculate_distances_from_all_gestures_knn(query_values, gestures):
+    distances = {}
+    minimum_distance = 100
+    similar_gesture = None
+    for gesture_id, gesture_values in gestures.items():
+        distance = calculate_distance_knn(query_values, gesture_values)
+        if distance < minimum_distance:
+            minimum_distance = distance
+            similar_gesture = gesture_id
+        distances[gesture_id] = distance
+    return similar_gesture
+
+def KNN(unlabeled_gestures, gestures):
+    predictions = {}
+    for query_id, query_values in unlabeled_gestures.items():
+        prediction_id = calculate_distances_from_all_gestures_knn(query_values, gestures)
+        predictions[query_id] = prediction_id
+    return predictions
+
+def split_data(gestures, all_labels, training_labels):
+    unlabeled_gestures = {}
+    labeled_gestures = gestures.copy()
+    for gesture_id in training_labels['id']:
+        unlabeled_gestures[gesture_id] = gestures[str(gesture_id)]
+        del labeled_gestures[str(gesture_id)]
+    return unlabeled_gestures, labeled_gestures
+
+def make_shift_accuracy(predictions):
+    correct = 0
+    for truth, prediction in predictions.items():
+        if abs(truth - int(prediction)) < 100:
+            correct += 1
+    accuracy = correct / len(predictions)
+    return accuracy
+        
+def main(): 
+    training_labels_dir = 'data/sample_training_labels.xlsx'
+    all_labels_dir = 'data/all_labels.xlsx'
+    training_labels = read_labels(training_labels_dir)
+    all_labels = read_labels(all_labels_dir)
+
+    gestures = task1.calculate_pca(1, 4)[0]
+    unlabeled_gestures, labeled_gestures = split_data(gestures, all_labels, training_labels)
+    predictions = KNN(unlabeled_gestures, labeled_gestures)
+    accuracy = make_shift_accuracy(predictions)
+    print("KNN accuracy: ", accuracy)
+
+if __name__ == '__main__':
+    main()
