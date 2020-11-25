fd70f3060549e6a515f64bddf75c58ca3d33b8c0
Jay Nankani <39469570+jaysn1@users.noreply.github.com>
2020-11-24 18:43:53 -0700

    Added task2 output

--- task2.py
+++ task2.py
@@ -34,14 +34,15 @@ def calculate_distances_from_all_gestures_knn(query_values, gestures):
         distances[gesture_id] = distance
     return similar_gesture
 
-def KNN(unlabeled_gestures, gestures):
-    predictions = {}
+def KNN(unlabeled_gestures, gestures, training_labels):
+    predictions = []
     for query_id, query_values in unlabeled_gestures.items():
         prediction_id = calculate_distances_from_all_gestures_knn(query_values, gestures)
-        predictions[query_id] = prediction_id
-    return predictions
+        temp = [query_id, training_labels[training_labels['id'] == prediction_id].label.values[0], training_labels[training_labels['id'] == prediction_id].label_code.values[0]]
+        predictions.append(temp)
+    return pd.DataFrame(predictions, columns=('id', 'label', 'label_code'))
 
-def split_data(gestures, all_labels, training_labels):
+def split_data(gestures, training_labels):
     labeled_gestures = {}
     unlabeled_gestures = gestures.copy()
     for gesture_id in training_labels['id']:
@@ -69,7 +70,7 @@ class Node:
     
 
 class DecisionTreeClassifier:
-    def __init__(self, max_depth=None):
+    def __init__(self, max_depth=0):
         self.max_depth = max_depth
         self.y_count = 0
         self.dimensionality = 0
@@ -81,7 +82,8 @@ class DecisionTreeClassifier:
         self.root = self.extend(X, y)
     
     def predict(self, X):
-        return [self.predict_single(inputs) for inputs in X]
+        mapping = {0:"combinato", 1:"daccordo", 2:"vattene"}
+        return pd.DataFrame([[key, mapping[self.predict_single(value)], self.predict_single(value)] for key, value in X.items()], columns=('id', 'label', 'label_code'))
     
     def get_split(self, X, y):
         m = y.size
@@ -132,56 +134,72 @@ class DecisionTreeClassifier:
 
 
 def main(): 
-    training_labels_dir = 'data/sample_training_labels.xlsx'
-    all_labels_dir = 'data/all_labels.xlsx'
+    training_labels_dir = 'data/labels.xlsx'
+    # all_labels_dir = 'data/all_labels.xlsx'
     training_labels = read_labels(training_labels_dir)
+<<<<<<< HEAD
+    # all_labels = read_labels(all_labels_dir)
+    # test_labels = all_labels[~all_labels['id'].isin(training_labels['id'])]
+=======
     all_labels = read_labels(all_labels_dir)
     test_labels = all_labels[~all_labels['id'].isin(training_labels['id'])].copy()
+>>>>>>> 7c0f52a4b63ea431e42834d77b27edd91a3d08a3
     
     training_labels['label'] = training_labels['label'].astype('category')
     training_labels['label_code'] = training_labels['label'].cat.codes
+    # training_labels = training_labels.set_index('id')
     
-    test_labels['label'] = test_labels['label'].astype('category') # TODO: Label according to training data
-    test_labels['label_code'] = test_labels['label'].cat.codes
+    # test_labels['label'] = test_labels['label'].astype('category') 
+    # test_labels['label_code'] = test_labels['label'].cat.codes
     
     gestures = task1.calculate_pca(1, 4)[0]
-    unlabeled_gestures, labeled_gestures = split_data(gestures, all_labels, training_labels)
-    X_train, X_test, y_train, y_test = pd.DataFrame(labeled_gestures.values()).to_numpy(), pd.DataFrame(unlabeled_gestures.values()).to_numpy(),training_labels['label_code'].to_numpy(),test_labels['label_code'].to_numpy()
+    unlabeled_gestures, labeled_gestures = split_data(gestures, training_labels)
+    X_train, X_test, y_train = pd.DataFrame(labeled_gestures.values()).to_numpy(), pd.DataFrame(unlabeled_gestures.values()).to_numpy(),training_labels['label_code'].to_numpy()
     
-    predictions = KNN(unlabeled_gestures, labeled_gestures)
-    accuracy = make_shift_accuracy(predictions)
-    print("KNN accuracy: ", accuracy)
-
+    predictions_knn = KNN(unlabeled_gestures, labeled_gestures, training_labels)
+    predictions_knn.to_csv("output/KNN_predictions.csv")
+    print("kNN results saved in output/KNN_predictions.csv")
+    # accuracy = make_shift_accuracy(predictions)
+    
+    # print("KNN accuracy: ", accuracy)
+    
     clf = DecisionTreeClassifier(max_depth=2)
     clf.fit(X_train, y_train)
-    y_pred_test = clf.predict(X_test)
-    print("Decision Tree accuracy: ", float(sum([1 if _y == _y_pred else 0 for _y, _y_pred in zip(y_pred_test, y_test)])) / len(y_pred_test))
-
+    y_pred_test = clf.predict(unlabeled_gestures)
+    y_pred_test.to_csv("output/DT_predictions.csv")
+    print("Decision Tree results saved in output/DT_predictions.csv")
+    
+    # print("Decision Tree accuracy: ", float(sum([1 if _y == _y_pred else 0 for _y, _y_pred in zip(y_pred_test, y_test)])) / len(y_pred_test))
+    
     # Code to call PPR classifier
     # inp = int(input("Do you want to create vector model for gestures (0/1)?: "))
     user_option = int(input("\n 1: DOT\n 2: PCA\n 3: SVD\n 4: NMF\n 5: LDA\n 6: ED \n7: DTW\n What to use for gesture gesture matrix? "))
     if user_option not in [6,7]:
         vector_model = int(input("What vector model to use (TF: 0, TF-IDF:1)?: "))
-
+    
     data = task1_initial_setup(user_option, vector_model, False)
     graph = pd.DataFrame.from_dict(data).values
-
+    
     labels = set()
-
+    
     # Labelled dataset for training
     input_image_label_pair = []
     tr_labels = read_files(training_labels_dir)
     d = {}
     for i in tr_labels[0]:
         d[tr_labels[0][i]] = str(tr_labels[1][i])
-
+    
+    mapping = {"combinato":0, "daccordo":1, "vattene":2}
     for line in d:
         image_id = str(line)
         label = d[line]
         labels.add(label)
-        input_image_label_pair.append([image_id, label])
-
-    accuracy = classifier(graph, labels, input_image_label_pair, all_labels_dir)
-
-    print("Personalised PageRank Classifier accuracy: " + str(accuracy))
+        input_image_label_pair.append([image_id, label, mapping[label]])
+    # print("PPR======================")
+    # print(input_image_label_pair)
     
+    accuracy = classifier(graph, labels, input_image_label_pair, training_labels_dir)
+    print("PPR results saved in output/PPR_predictions.csv")
+    
+    # print("Personalised PageRank Classifier accuracy: " + str(accuracy))
+        
